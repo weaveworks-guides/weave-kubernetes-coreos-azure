7e70988dd6ad93ad86eb82593ef739f36da113e0
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-04-04 15:28:37 +0100

    Updates for Kubernetes 1.2
    - bump CoreOS images to latest
    - bump JavaScript dependencies
    - fix few bugs in provisioning scripts
    - band-aid weaveworks/weave#2094

--- scale-kubernetes-cluster.js
+++ scale-kubernetes-cluster.js
@@ -5,6 +5,8 @@ var kube = require('./lib/deployment_logic/kubernetes.js');
 
 azure.load_state_for_resizing(process.argv[2], 'kube', parseInt(process.argv[3] || 1));
 
+var coreos_update_channel = process.env['AZ_VM_COREOS_CHANNEL'] || 'stable';
+
 azure.run_task_queue([
-  azure.queue_machines('kube', 'stable', kube.create_node_cloud_config),
+  azure.queue_machines('kube', coreos_update_channel, kube.create_node_cloud_config),
 ]);
